97925c2c657d4ce9628285c4a4a33c9505a62dc7
lpwj <learnprogrammingwithjoel@gmail.com>
2022-09-18 16:42:07 +0100

    adding first spring boot files

---
+++ ojet.tutorials/src/test/java/lpwj/ojet/tutorials/ojet/tutorials/ApplicationTests.java
@@ -0,0 +1,13 @@
+package lpwj.ojet.tutorials.ojet.tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
